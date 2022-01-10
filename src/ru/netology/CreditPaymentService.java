ef6a040812f3c702ef4722cc7b2497cb94003964
PetrIvChe <cherniavskiipetr@gmail.com>
2022-01-10 12:31:13 +0300

    Значит дело не в double, а не верно записанной формуле

--- src/ru/netology/CreditPaymentService.java
+++ src/ru/netology/CreditPaymentService.java
@@ -2,12 +2,6 @@ package ru.netology;
 
 public class CreditPaymentService {
     public String calculate(double sum, double percent, int term) {
-//        double r = Math.pow(1 + percent / 100, 1.0 / 12.0);
-//        double q = Math.pow(r, 12.0 * term);
-//        double a = (sum * q * (r - 1)) / (q - 1);
-//        String monthlyPayment = String.format("%.0f", a);
-//        return monthlyPayment;
-
         double r = percent / (100 * 12);
         double q = Math.pow(1 + r, -12 * term);
         double a = sum * (r / (1 - q));
