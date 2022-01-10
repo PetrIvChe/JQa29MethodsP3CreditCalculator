623e1ae84dfa90b3a17cebe69dbc62100f8469a1
PetrIvChe <cherniavskiipetr@gmail.com>
2022-01-10 12:03:27 +0300

    По формуле с корнями большая погрешность из-за double

---
+++ src/ru/netology/Main.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        String monthlyPayment1year = service.calculate(1_000_000, 9.99, 1);
+        System.out.println("Ежемесячный платеж составит: " + monthlyPayment1year + " при сроке кредита 1 год");
+        String monthlyPayment2year = service.calculate(1_000_000, 9.99, 2);
+        System.out.println("Ежемесячный платеж составит: " +  monthlyPayment2year + " при сроке кредита 2 года");
+        String monthlyPayment3year = service.calculate(1_000_000, 9.99, 3);
+        System.out.println("Ежемесячный платеж составит: " + monthlyPayment3year + " при сроке кредита 3 года");
+    }
+}
